0c2cb68cf88133fd11bac002937984af49f643d1
JesusAbarcaRodriguez <jesus.abarca.rodriguez@est.una.ac.cr>
2024-05-11 11:07:37 -0600

    [CREATE INITIAL STRUCTURE]
    Se crearon las carpetas model, view, controller.
    Apenas se empieza el proyecto.

---
+++ model/process.py
@@ -0,0 +1,8 @@
+class Proceso:
+    def __init__(self, name,priority,total_time_finish,executed_time,waiting_time,):
+        self.name = name
+        self.priority = priority
+        self.total_time_finish = total_time_finish
+        self.executed_time = executed_time
+        self.waiting_time = waiting_time
+
