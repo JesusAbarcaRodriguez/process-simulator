3b2eeee5cb5ee10e68d1266bcc04d76bb0512a06
JesusAbarcaRodriguez <121053502+JesusAbarcaRodriguez@users.noreply.github.com>
2024-05-14 21:36:25 -0600

    [CHANGE view_controller data_structure]
    Ahora, no se trabaja con una lista de procesos y otra lista para bloques para cada tipo de memoria.
    Ahora se hacen 2 instancias de memoria y dentro de ella se trabaja con la lista de bloques que corresponde.
    Se cambiaron los métodos de memory y del controlador por cuestiones de cambios.

--- model/memory.py
+++ model/memory.py
@@ -33,27 +33,29 @@ class Memory:
     #   Assign memory to processes
     #   order memory blocks by size
     #   assign memory blocks to processes
-    def assign_memory(self, process: Process, block_memory_list):
-        sorted(block_memory_list,key=lambda x: x.size)
-        allocated_processes = []
-        for i in range(0,len(block_memory_list) -1 ):
-            for z in range(0,len(process) -1):
-                if block_memory_list[i].proc is None and block_memory_list[i].size >= process[z].size:
-                    block_memory_list[i].proc = process[z]
-                    allocated_processes.append(process[z])
-                    break
-        primary_process = process
-        for i in range(0,len(allocated_processes) -1):
-            for z in range(0,len(process) -1):
-                if allocated_processes[i].pid == process[z].pid:
-                    process.remove(allocated_processes[i])
-        return primary_process, block_memory_list,process
+    def assign_memory(self,block_secondary_list,block_primary_list):
+        is_assigned = False
+        for block_secondary in block_secondary_list:
+            if block_secondary.proc is not None:
+                for block_primary in block_primary_list:
+                    if block_primary.proc is None and block_primary.size >= block_secondary.proc.size:
+                        block_primary.proc = block_secondary.proc
+                        block_secondary.proc = None
+                        is_assigned = True
+                        break
+        return block_secondary_list,block_primary_list,is_assigned       
 
     # todo: new, suspended_ready, suspended_blocked
-    def assign_memory_secondary(self,process,block_memory_list):
-        sorted(block_memory_list,key=lambda x: x.size)
-        for i in range(0,len(block_memory_list) -1 ):
-            for z in range(0,len(process) -1):
-                if block_memory_list[i].proc is None and block_memory_list[i].size >= process[z].size:
-                    block_memory_list[i].proc = process[z]
-        return process,block_memory_list
+    def assign_memory_secondary(self, process, block_memory_list):
+        for process_item in process:
+            for block in block_memory_list:
+                if block.proc is None and block.size >= process_item.size:
+                    block.proc = process_item
+                    break
+        return block_memory_list
+    def assign_proc_to_memory_secondary(self,proc,block_memory_list):
+        for block in block_memory_list:
+            if block.proc is None and block.size >= proc.size:
+                block.proc = proc
+                break
+        return block_memory_list
