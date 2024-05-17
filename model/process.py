71d4bc6f5ad828628e5b880c4345af2972e27d5d
Daniel Araya <darayaroma@gmail.com>
2024-05-17 00:10:55 -0600

    [FIX] Integrating Process State Logic with the UI, to automate the program

--- model/process.py
+++ model/process.py
@@ -1,3 +1,4 @@
+import random
 import threading
 import time
 from util.states import ProcessState
@@ -14,7 +15,7 @@ class Process:
         self.executed_time = executed_time
         self.waiting_time = waiting_time
         self.thread = ProcessState.NEW
-        self.lock = threading.Lock()
+        # self.lock = threading.Lock()
 
     def __str__(self):
         return f"Process {self.pid}: {self.name}"
@@ -22,18 +23,15 @@ class Process:
     #   Admit the process
     #   Changes the state from new to ready
     def admit(self):
-        with self.lock:
-            if self.state == ProcessState.NEW:
-                self.ready()
-                self.run()
+        if self.state == ProcessState.READY:
+            self.run()
 
     #   Starts a thread to execute the process
     def run(self):
-        with self.lock:
-            if self.state == ProcessState.READY:
-                self.state = ProcessState.RUNNING
-                thread = threading.Thread(target=self.execute)
-                thread.start()
+        if self.state == ProcessState.READY:
+            self.state = ProcessState.RUNNING
+            thread = threading.Thread(target=self.execute)
+            thread.start()
 
     #   todo: timeout -> (running) -> (ready)
     #   calculate to_finish_time
@@ -41,68 +39,60 @@ class Process:
     #   Execute the process
     #   When the process is ready, it will start executing
     def execute(self, event=None):
-        while self.executed_time < self.to_finish_time:
-            with self.lock:
-                if event and event.is_set():
-                    self.block(event)
-                else:
-                    self.executed_time += 1
-                    self.waiting_time += 1
-                    time.sleep(1)
+        while self.executed_time < self.to_finish_time and self.state == ProcessState.RUNNING:
+                # if event and event.is_set():
+                    # self.block(event)
+                # else:
+            self.executed_time += round(random.uniform(1, 3), 2)
+            time.sleep(1)
             if self.executed_time == self.to_finish_time:
+                time.sleep(1)
                 self.terminate()
 
     #   todo: suspend -> (blocked) -> (suspended_blocked)
     #   Block the process
     #   Changes the state from running to blocked
     def block(self, event=None):
-        with self.lock:
-            if self.state == ProcessState.RUNNING:
-                self.state = ProcessState.BLOCKED
-                if event:
-                    event.clear()
-                    event.wait()
-                    self.ready()
+        if self.state == ProcessState.RUNNING:
+            self.state = ProcessState.BLOCKED
+            if event:
+                event.clear()
+                event.wait()
+                self.ready()
 
     #   todo: suspend -> (ready) -> (suspended_ready)
     #   Prepare the process
     #   Changes the state from new, blocked, suspended_ready to ready 
     def ready(self):
-        with self.lock:
-            if self.state in [ProcessState.NEW, ProcessState.SUSPENDED_READY, ProcessState.BLOCKED]:
-                self.state = ProcessState.READY 
+        if self.state in [ProcessState.NEW, ProcessState.SUSPENDED_READY, ProcessState.BLOCKED]:
+            self.state = ProcessState.READY 
 
     #   Activate the process
     #   Changes the state from suspended to ready/block
     #   - If the process is suspended_blocked, it will change to blocked
     #   - If the process is suspended_ready, it will change to ready
     def activate(self):
-        with self.lock:
-            if self.state == ProcessState.SUSPENDED_BLOCKED:
-                self.state = ProcessState.BLOCKED
-            elif self.state == ProcessState.SUSPENDED_READY:
-                self.ready()
+        if self.state == ProcessState.SUSPENDED_BLOCKED:
+            self.state = ProcessState.BLOCKED
+        elif self.state == ProcessState.SUSPENDED_READY:
+            self.ready()
 
     #   Suspend the process
     #   Changes the state from blocked to suspended_blocked
-    def suspended_block(self, event):
-        with self.lock:
-            if self.state == ProcessState.SUSPENDED_BLOCKED and not event.is_set():
-                event.wait()
-                self.state = ProcessState.SUSPENDED_READY
+    def suspended_block(self):
+        if self.state == ProcessState.BLOCKED or self.state == ProcessState.RUNNING:
+            self.state = ProcessState.SUSPENDED_BLOCKED
 
     #   Suspend the process
     #   Changes the state from ready to suspended_ready
     def suspended_ready(self):
-        with self.lock:
-            if self.state == ProcessState.READY:
-                self.state = ProcessState.SUSPENDED_READY
+        if self.state == ProcessState.READY:
+            self.state = ProcessState.SUSPENDED_READY
 
 
     #   Terminate the process
     #   Changes the state from running to terminated
     def terminate(self):
-        with self.lock:
-            if self.state == ProcessState.RUNNING:
-                self.state = ProcessState.TERMINATED
-                self.thread.join()
+        if self.state == ProcessState.RUNNING:
+            self.state = ProcessState.TERMINATED
+            self.thread.join()
