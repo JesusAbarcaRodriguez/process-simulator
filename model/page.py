b00fac0050a5fb52a640dc502dca1f5dc718da13
JesusAbarcaRodriguez <121053502+JesusAbarcaRodriguez@users.noreply.github.com>
2024-05-23 13:17:36 -0600

    Create page.py
    Se creo la clase Page.py

---
+++ model/page.py
@@ -0,0 +1,6 @@
+class Page:
+    def __init__(self, page_id, size, process, page_number):
+        self.page_id = page_id
+        self.size = size
+        self.process = process
+        self.page_number = page_number
