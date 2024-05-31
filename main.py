ec8923c9be3fd27646e6336f97564f5478d39f0e
JesusAbarcaRodriguez <121053502+JesusAbarcaRodriguez@users.noreply.github.com>
2024-05-31 15:32:16 -0600

    Update main.py
    Ahora la aplicación empieza en el menu.ui

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from controller.view_controller import MainView
+from controller.menu_controller import MenuApp
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -8,7 +8,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 # Main
 # =======================
 app = QApplication(sys.argv)
-main = MainView()
+main = MenuApp()
 main.show()
 
 sys.exit(app.exec_())
